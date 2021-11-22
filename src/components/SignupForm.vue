9eaa1f6e018c245189625ef0d24e2019ac477f06
Jeff Jessie <j3ffjessie@protonmail.com>
2021-11-22 14:15:57 -0600

    Add inputs for email and password. Track input using v-model that two way binds the input from the input field to the data and from the data property to the input field. Also added in dropdown select box for job role to track that as well.

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <form>
+      <label>Email:</label>
+      <input type="email" v-model='email' required />
+      
+      <label>Password:</label>
+      <input type="password" v-model='password' required />
+
+      <label>Role:</label>
+      <select v-model='role'>
+          <option value='Developer'>Web Developer</option>
+          <option value='Designer'>Web Designer</option>
+      </select>
+      
+  </form>
+  <p>Email: {{ email}} </p>
+  <p>Password: {{ password }}</p>
+  <p>Job Role: {{ role }}</p>
+</template>
+
+<script>
+
+export default {
+    data() {
+        return {
+            email: '',
+            password: '',
+            role: ''
+        }
+    },
+}
+</script>
+
+<style>
+form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+}
+
+label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+}
+
+input {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+}
+</style>
